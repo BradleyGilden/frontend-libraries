dd7cf28b0626f22e339b7e6a8246e9f083c57f33
bradleygilden <bradleygilden@gmail.com>
2024-04-02 12:34:56 +0000

    scroll trigger and pinning

--- GSAP/index.js
+++ GSAP/index.js
@@ -1,12 +1,12 @@
 import { gshift } from "./tween.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger,ScrollToPlugin,EaselPlugin,TextPlugin);
+    gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
 
     const screenWidth = window.innerWidth;
 
     // Calculate 100vw - 10rem
-    const adjustedWidth = screenWidth - (10 * parseFloat(getComputedStyle(document.documentElement).fontSize));
+    const adjustedWidth = screenWidth - (12 * parseFloat(getComputedStyle(document.documentElement).fontSize));
     
     // bouncing square
     gshift(gsap, '.square', 'to', {
@@ -49,6 +49,40 @@ document.addEventListener("DOMContentLoaded", (event) => {
             borderRightColor: 'rgba(255,255,255,0)',
             repeat: -1,
             ease: 'steps(29)'
+        }
+    })
+
+    // typewriter2 effect with scrolling:
+    gshift(gsap, '#typewriter2', 'fromTo', {
+        duration: 6,
+        from: {
+            width: '0',
+        },
+        to: {
+            scrollTrigger: {
+                trigger: '#container',
+                start: 'bottom center',
+                end: 'top top',
+                scrub: 2,
+                toggleActions: 'play pause resume reset',
+                pin: true,
+                markers: {startColor: "white", endColor: "white", fontSize: "18px", fontWeight: "bold", indent: 20},
+            },
+            width: '24.09rem',
+            ease: 'steps(29)',
+        }
+    });
+
+    // cursor
+    gshift(gsap, '#typewriter2', 'fromTo', {
+        duration: 0.5,
+        from: {
+            borderRightColor: 'rgba(255,255,255,0.75)',
+        },
+        to: {
+            borderRightColor: 'rgba(255,255,255,0)',
+            repeat: -1,
+            ease: 'steps(29)',
         }
     })
 });
