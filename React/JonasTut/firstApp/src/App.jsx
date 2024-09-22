1f69f82e51d304fd2d31749a048304a8d7fe0e09
bradleygilden <bradleygilden@gmail.com>
2024-09-22 21:01:40 +0000

    simple app

--- React/JonasTut/firstApp/src/App.jsx
+++ React/JonasTut/firstApp/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [advice, setAdvice] = useState('Hello World')
+
+  async function getAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    setAdvice(data.slip.advice)
+  }
 
   return (
-    <>
-    <h1>Hello World</h1>
+    <>  
+    <h1>{advice}</h1>
+    <button onClick={getAdvice}>get advice</button>
     </>
   )
 }
